d3ad40f80c71b9a7b3b7db6c676dae9b8a325123
Anushka Pandey <76942261+AnushkaPandey-021@users.noreply.github.com>
2024-10-25 13:49:23 +0530

    Delete Python directory

--- Python/maxFunction.py
+++
@@ -1,4 +0,0 @@
-number =input("enter a list of numbers: ")
-list_num = [int(num) for num in number.split()]
-largest = max(list_num)
-print(largest)
--- Python/sum.py
+++
@@ -1,6 +0,0 @@
-x=int(input("Enter value of first number:"/t))
-y=int(input("Enter value of second number:"/t))
-
-z= x+y
-
-print("Sum of the numbers is:",z)
